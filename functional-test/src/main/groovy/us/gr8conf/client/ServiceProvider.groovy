3b78e96827177f8dc466ce8066ba9b2312fcfa08
Chris Smalley <chrissmalley@me.com>
2017-07-09 14:43:09 -0500

    functional test zipkin integration example

--- functional-test/src/main/groovy/us/gr8conf/client/ServiceProvider.groovy
+++ functional-test/src/main/groovy/us/gr8conf/client/ServiceProvider.groovy
@@ -1,7 +1,10 @@
 package us.gr8conf.client
 
+import brave.http.HttpTracing
+import brave.okhttp3.TracingInterceptor
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import okhttp3.Dispatcher
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -18,21 +21,31 @@ class ServiceProvider {
     static ObjectMapper objectMapper = new ObjectMapper()
                     .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
-    static Service service(String url) {
+    static Service service(String url, HttpTracing httpTracing) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-                .client(okHttpClient())
+                .client(okHttpClient(httpTracing))
                 .build()
 
         return RetrofitRequestLoggingProxy.create(url, retrofit.create(Service))
     }
 
-    private static OkHttpClient okHttpClient() {
-        new OkHttpClient().newBuilder()
+    private static OkHttpClient okHttpClient(HttpTracing httpTracing) {
+        return new OkHttpClient().newBuilder()
                 .addInterceptor(correlationLoggingInterceptor())
                 .addInterceptor(httpLoggingInterceptor())
+                .dispatcher(zipkinHttpTracingDispatcher(httpTracing))
+                .addNetworkInterceptor(zipkinTracingInterceptor(httpTracing))
                 .build()
+    }
+
+    private static Dispatcher zipkinHttpTracingDispatcher(HttpTracing httpTracing) {
+        return new Dispatcher(httpTracing.tracing().currentTraceContext().executorService(new Dispatcher().executorService()))
+    }
+
+    private static Interceptor zipkinTracingInterceptor(HttpTracing httpTracing) {
+        return TracingInterceptor.create(httpTracing)
     }
 
     private static Interceptor correlationLoggingInterceptor() {
--- functional-test/src/test/groovy/us/gr8conf/FunctionalTestSpecification.groovy
+++ functional-test/src/test/groovy/us/gr8conf/FunctionalTestSpecification.groovy
@@ -1,20 +1,62 @@
 package us.gr8conf
 
+import brave.Span
+import brave.Tracer
+import brave.Tracing
+import brave.http.HttpTracing
+import brave.sampler.Sampler
 import com.anotherchrisberry.spock.extensions.retry.RetryOnFailure
 import spock.lang.Shared
 import spock.lang.Specification
 import us.gr8conf.client.Service
 import us.gr8conf.client.ServiceProvider
 import us.gr8conf.mdc.SpecMDC
+import zipkin.reporter.Reporter
 
 @SpecMDC
 @RetryOnFailure(times=2)
 class FunctionalTestSpecification extends Specification {
 
     @Shared Service service
+    @Shared HttpTracing httpTracing
+
+    Span span
+    Tracer.SpanInScope spanInScope
 
     def setupSpec() {
-        service = ServiceProvider.service(serviceUrl())
+        Tracing tracing = Tracing.newBuilder()
+                .localServiceName("service")
+                .reporter(getSpanReporter())
+                .sampler(Sampler.ALWAYS_SAMPLE)
+                .build()
+
+        httpTracing = HttpTracing.create(tracing)
+
+        service = ServiceProvider.service(serviceUrl(), httpTracing)
+    }
+
+    void cleanupSpec() {
+        // No zipkin reporter to call flush on, since there is no Zipkin server to write too.
+        // reporter.flush()
+    }
+
+    def setup() {
+        span = httpTracing.tracing().tracer().newTrace()
+                .kind(Span.Kind.SERVER)
+                .name(specificationContext.currentSpec.name + "-" + specificationContext.currentFeature.name)
+                .start()
+        spanInScope = httpTracing.tracing().tracer().withSpanInScope(span)
+    }
+
+    void cleanup() {
+        span.finish()
+        spanInScope.close()
+    }
+
+    //There is not a Zipkin server to report spans too, since this is an example project.
+    //In the real world, you would build up a reporter here.
+    Reporter<zipkin.Span> getSpanReporter() {
+        return Reporter.NOOP
     }
 
     def serviceUrl() {
